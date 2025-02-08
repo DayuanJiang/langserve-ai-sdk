89c278a111f436be5322e8874fd5f2f9864d8ead
Mutsuki Yamamoto <mutsuki349@gmail.com>
2025-02-08 23:40:37 +0000

    uiの変更をしました

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { readStreamableValue } from "ai/rsc";
 import { runAgent } from "./actions";
 import { StreamEvent } from "@langchain/core/tracers/log_stream";
 import Tabs from "./components/Tabs";
+import TextInputForm from "./components/TextInputForm";
+import ClipboardCopy from "./components/ClipboardCopy";
 
 export default function Page() {
     const [input, setInput] = useState("");
@@ -54,26 +56,11 @@ export default function Page() {
 
     return (
         <div className="flex flex-col w-full gap-2">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-1/3 m-10 relative">
-            <textarea
-                className="w-full h-40 px-5 py-2 pb-10 outline-none resize-none rounded-3xl shadow-lg"
-                placeholder="文章を入力して"
-                value={input}
-                onChange={(e) => setInput(e.target.value)} 
-            />
-             <button
-            type="submit"
-            className={`absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center ${input ? "bg-slate-700" : "bg-gray-400 cursor-not-allowed"}`}
-            disabled={!input}
-        >
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 24 24">
-                <path fill="currentColor" d="M3 20V4l19 8M5 17l11.85-5L5 7v3.5l6 1.5l-6 1.5M5 17V7v6.5Z" />
-            </svg>
-        </button>
-            </form>
-
+            
+            <TextInputForm/>
             <Tabs />  
-
+            <ClipboardCopy/>
+            <ClipboardCopy/>
             <div className="flex flex-col w-full gap-2">
                 <div
                     className="flex flex-col gap-2 px-2 h-[650px] overflow-y-auto"
