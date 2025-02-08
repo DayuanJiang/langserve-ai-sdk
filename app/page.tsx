4c15a9fa0aa0d01c7137350221d1a86a1ed4fde5
Mutsuki Yamamoto <mutsuki349@gmail.com>
2025-02-08 14:13:44 +0000

    uiを変えました

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { readStreamableValue } from "ai/rsc";
 import { runAgent } from "./actions";
 import { StreamEvent } from "@langchain/core/tracers/log_stream";
-
+import Tabs from "./components/Tabs";
 
 export default function Page() {
     const [input, setInput] = useState("");
@@ -54,20 +54,26 @@ export default function Page() {
 
     return (
         <div className="flex flex-col w-full gap-2">
-            <form onSubmit={handleSubmit} className="flex flex-col w-full gap-2">
-                <input
-                    type="text"
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    className="border border-gray-300 rounded-md p-2 mr-2 w-full"
-                />
-                <button
-                    type="submit"
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                >
-                    Run
-                </button>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-1/3 m-10 relative">
+            <textarea
+                className="w-full h-40 px-5 py-2 pb-10 outline-none resize-none rounded-3xl shadow-lg"
+                placeholder="文章を入力して"
+                value={input}
+                onChange={(e) => setInput(e.target.value)} 
+            />
+             <button
+            type="submit"
+            className={`absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center ${input ? "bg-slate-700" : "bg-gray-400 cursor-not-allowed"}`}
+            disabled={!input}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M3 20V4l19 8M5 17l11.85-5L5 7v3.5l6 1.5l-6 1.5M5 17V7v6.5Z" />
+            </svg>
+        </button>
             </form>
+
+            <Tabs />  
+
             <div className="flex flex-col w-full gap-2">
                 <div
                     className="flex flex-col gap-2 px-2 h-[650px] overflow-y-auto"
@@ -96,16 +102,13 @@ export default function Page() {
                                     if (item.output === "") return null;
                                     return (
                                         <div key={i} className="p-4 bg-slate-100 rounded-lg prose">
-
                                             {item.output}
                                         </div>
                                     );
                                 default:
                                     return null;
                             }
-
                         })
-
                     }
                 </div>
             </div>
