b9c7bdfce2bd2a31316d0f3ef31ae590c1533bb2
大金有羽 <yuogane@ookaneyuuwanoMacBook-Air.local>
2025-02-15 00:26:05 +0900

    Implement loading animation

--- app/components/TextInputForm.tsx
+++ app/components/TextInputForm.tsx
@@ -1,59 +1,69 @@
 import React from "react";
 import LoadingDots from "@/app/components/loadingAnimation";
 
-
 type formProps = {
-    input : string;
-    handleSubmit : (e: React.FormEvent) => void;
-    setInput : (value: string) => void;
-    loading : boolean;
-}
+  input: string;
+  handleSubmit: (e: React.FormEvent) => void;
+  setInput: (value: string) => void;
+  loading: boolean;
+};
 
-const TextInputForm = ({input,handleSubmit,setInput,loading}:formProps) => {
-
-  const Button = (loading: boolean,input:string) => {
-
+const TextInputForm = ({
+  input,
+  handleSubmit,
+  setInput,
+  loading,
+}: formProps) => {
+  const Button = (loading: boolean, input: string) => {
     if (loading) {
-        return (
-            <button
-                type="submit"
-                className="absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center bg-gray-400 cursor-not-allowed"
-                disabled
-            >
-                <LoadingDots />
-            </button>  
-            )
-    } else {
-        return (
-          <button
+      return (
+        <button
           type="submit"
-          className={`absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center ${input ? "bg-sky-400 dark:bg-slate-600" : "bg-gray-400 cursor-not-allowed"}`}
+          className="absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center bg-gray-400 cursor-not-allowed"
+          disabled
+        >
+          <LoadingDots />
+        </button>
+      );
+    } else {
+      return (
+        <button
+          type="submit"
+          className={`absolute right-0 bottom-4 mb-1 mr-4 w-10 h-10 rounded-full flex items-center justify-center ${
+            input
+              ? "bg-sky-400 dark:bg-slate-600"
+              : "bg-gray-400 cursor-not-allowed"
+          }`}
           disabled={!input}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M3 20V4l19 8M5 17l11.85-5L5 7v3.5l6 1.5l-6 1.5M5 17V7v6.5Z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 text-black"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M3 20V4l19 8M5 17l11.85-5L5 7v3.5l6 1.5l-6 1.5M5 17V7v6.5Z"
+            />
           </svg>
         </button>
-    )
+      );
     }
-    
-}
+  };
 
   return (
     <div className="flex flex-col w-full gap-2">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2 w-full  relative">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2 w-full  relative"
+      >
         <textarea
-          className="w-full h-40 px-5 py-3 pb-10 outline-none resize-none rounded-3xl shadow-lg dark:bg-slate-700 dark:text-white"
+          className="w-full h-40 px-5 py-3 pb-10 outline-none resize-none rounded-3xl shadow-lg text-[#334155] dark:bg-slate-700 dark:text-white"
           placeholder="文章を入力して下さい"
           value={input}
-          onChange={(e) => setInput(e.target.value)} 
+          onChange={(e) => setInput(e.target.value)}
         />
-        <div>
-          {Button(loading,input)}
-          
-        </div>
-
-        
+        <div>{Button(loading, input)}</div>
       </form>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,135 +5,141 @@ import TextInputForm from "./components/TextInputForm";
 import ClipboardCopy from "./components/ClipboardCopy";
 import VideoComponent from "@/app/components/VIdeo";
 import Headers from "@/app/components/header";
-import { examplePrompt,examplePromptfilter } from "@/app/data/examplePrompt";
+import { examplePrompt, examplePromptfilter } from "@/app/data/examplePrompt";
 import DawnloadButton from "@/app/components/DownloadButton";
 import Code from "@/app/components/Code";
 import { v4 as uuid } from "uuid";
 import promptDesoger from "@/app/utils/fetchPromptDesoger";
 
-
 export default function Page() {
-    const [videoUrl, setVideoUrl] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const [userPrompt, setUserPrompt] = useState("");
-    const [activeTab, setActiveTab] = useState(0);
-    const [filteredExamplePrompt, setFilteredExamplePrompt] = useState(examplePrompt.generateAnimationPrompt);
-    const [code, setCode] = useState<string>("");
-    const [videoId, setVideoId] = useState<string>("test");
+  const [videoUrl, setVideoUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [userPrompt, setUserPrompt] = useState("");
+  const [activeTab, setActiveTab] = useState(0);
+  const [filteredExamplePrompt, setFilteredExamplePrompt] = useState(
+    examplePrompt.generateAnimationPrompt
+  );
+  const [code, setCode] = useState<string>("");
+  const [videoId, setVideoId] = useState<string>("test");
 
+  useEffect(() => {
+    setFilteredExamplePrompt(examplePromptfilter(examplePrompt, activeTab));
+  }, [activeTab]);
 
-    useEffect(()=>{
-        setFilteredExamplePrompt(examplePromptfilter(examplePrompt, activeTab))
-    },[activeTab])
+  async function synchronize_video() {
+    // このvideoIdはsession情報において少し考える必要がある。
+    setLoading(true);
+    const defulatpath = process.env.NEXT_PUBLIC_API_URL;
+    const path = defulatpath + "/api/get_script/" + videoId;
+    const response = await fetch(path, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
 
-    async function synchronize_video(){
-        // このvideoIdはsession情報において少し考える必要がある。
-        setLoading(true);
-        const defulatpath = process.env.NEXT_PUBLIC_API_URL
-        const path =  defulatpath+"/api/get_script/" + videoId;
-        const response = await fetch(path, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-        });
-
-        if (!response.ok) {
-            throw new Error("動画の取得に失敗しました");
-        }else{
-            const code = await response.text();
-            const formattedText = code
+    if (!response.ok) {
+      throw new Error("動画の取得に失敗しました");
+    } else {
+      const code = await response.text();
+      const formattedText = code
                                 .replace(/\\n/g, "\n")  // 🔹 \n を改行に変換
                                 .replace(/\\"/g, "\"")  // 🔹 \" を " に戻す
                                 .replace(/^"|"$/g, ""); // 🔹 先頭と末尾の " を削除
-            setCode(formattedText);
-        }
+      setCode(formattedText);
     }
+  }
 
-    async function handleSubmit(e: React.FormEvent) {
-        setLoading(true);
-        const defulatpath = process.env.NEXT_PUBLIC_API_URL
-        setVideoId(uuid());
-        console.log(defulatpath)
-        const path =  defulatpath+"/api/prompt";
-        e.preventDefault();
-        if (!userPrompt) return;
-        // 50文字未満の場合は記述量を多くする処理を行う
-        if(userPrompt.length <50){
-            const rewritePrompt = await promptDesoger({user_prompt:userPrompt,instruction_type:activeTab});
-            setUserPrompt(rewritePrompt);
-            setLoading(false);
-        }else{
-            try {
-                // 1. プロンプトを送信
-                const videoResponse = await fetch(path, {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ user_prompt: userPrompt, video_id: videoId ,instruction_type : activeTab}),
-                });
+  async function handleSubmit(e: React.FormEvent) {
+    setVideoUrl(null); // 初期化
+    setLoading(true);
+    const defulatpath = process.env.NEXT_PUBLIC_API_URL;
+    setVideoId(uuid());
+    console.log(defulatpath);
+    const path = defulatpath + "/api/prompt";
+    e.preventDefault();
+    if (!userPrompt) return;
+    // 50文字未満の場合は記述量を多くする処理を行う
+    if (userPrompt.length < 50) {
+      const rewritePrompt = await promptDesoger({
+        user_prompt: userPrompt,
+        instruction_type: activeTab,
+      });
+      setUserPrompt(rewritePrompt);
+      setLoading(false);
+    } else {
+      try {
+        // 1. プロンプトを送信
+        const videoResponse = await fetch(path, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            user_prompt: userPrompt,
+            video_id: videoId,
+            instruction_type: activeTab,
+          }),
+        });
 
-                if (!videoResponse.ok) {
-                    throw new Error("動画生成に失敗しました。");
-                }
-
-                const blob = await videoResponse.blob();
-                const url = URL.createObjectURL(blob);
-                setVideoUrl(url);
-                synchronize_video();
-            } catch (err) {
-                setError((err as Error).message);
-                console.log(error)
-            } finally {
-                setLoading(false);
-            }
+        if (!videoResponse.ok) {
+          throw new Error("動画生成に失敗しました。");
         }
-            
-    
+
+        const blob = await videoResponse.blob();
+        const url = URL.createObjectURL(blob);
+        setVideoUrl(url);
+        synchronize_video();
+      } catch (err) {
+        const errorMessage = (err as Error).message;
+        setError(errorMessage);
+        console.log(errorMessage);
+      } finally {
+        setLoading(false);
+      }
     }
+  }
 
-
-    return (
+  return (
     <div className="flex flex-col w-full gap-2 bg-gray-200 dark:bg-slate-600">
-            <Headers />
-        <div className="flex w-full gap-2 justify-center">
-            <div className="flex flex-col w-[40%] ">
-        <div className="mb-5">
-                    <Tabs activeTab={activeTab} setActiveTab={setActiveTab} /> 
-                </div>
+      <Headers />
+      {error && (
+        <div className="bg-red-500 text-white p-2 rounded">エラー: {error}</div>
+      )}
+      <div className="flex w-full gap-2 justify-center">
+        <div className="flex flex-col w-[40%] ">
+          <div className="mb-5">
+            <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
+          </div>
 
-                <div className="mx-2">
-                    <TextInputForm input={userPrompt}  handleSubmit={handleSubmit} setInput={setUserPrompt} loading={loading} />
-                </div>
-                
+          <div className="mx-2">
+            <TextInputForm
+              input={userPrompt}
+              handleSubmit={handleSubmit}
+              setInput={setUserPrompt}
+              loading={loading}
+            />
+          </div>
 
-                <div className="flex flex-col mt-4 mb-1 gap-y-4">
-                    {
-                        filteredExamplePrompt.map((prompt,index) => {
-                            return (
-                                <div className="flex flex-col gap-2" key={index}>
-                                    <ClipboardCopy defaultText={prompt.prompt} />
-                                </div>
-                            );
-                        })
-                    }
+          <div className="flex flex-col mt-4 mb-1 gap-y-4">
+            {filteredExamplePrompt.map((prompt, index) => {
+              return (
+                <div className="flex flex-col gap-2" key={index}>
+                  <ClipboardCopy defaultText={prompt.prompt} />
                 </div>
-                
-            </div>
-            <div className="flex flex-col w-[50%] ">
-                <div className="my-10 ml-20">
-                    <VideoComponent videoUrl={videoUrl}/>
-                </div>
-                <div className="ml-20">
-                    {
-                        videoUrl && <DawnloadButton videoUrl={videoUrl} />
-                    }
-                </div>
-                <div className="my-10 ml-20">
-                    <Code code={code} />
-                </div>
-            </div>
-
-        </div>   
+              );
+            })}
+          </div>
+        </div>
+        <div className="flex flex-col w-[50%] ">
+          <div className="my-10 ml-20">
+            <VideoComponent videoUrl={videoUrl} loading={loading} />
+          </div>
+          <div className="ml-20">
+            {videoUrl && <DawnloadButton videoUrl={videoUrl} />}
+          </div>
+          <div className="my-10 ml-20">
+            <Code code={code} />
+          </div>
+        </div>
+      </div>
     </div>
-        
-    );
+  );
 }
